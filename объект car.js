82839e6b4eb1997c1671397165992badad3f52e7
Alex Demin <49374958+Leshka92@users.noreply.github.com>
2019-04-17 16:32:57 +0300

    Рандомный объект автомобиля

---
+++ объект car.js
@@ -0,0 +1,51 @@
+/*
+ * Это простой редактор JavaScript.
+ *
+ * Введите JavaScript, затем щёлкните правой кнопкой или выберите из меню Выполнить:
+ * 1. Запустить, чтобы исполнить выделенный текст (Ctrl+R),
+ * 2. Исследовать, чтобы вызвать для результата Инспектор Объектов (Ctrl+I), или,
+ * 3. Отобразить, чтобы вставить результат в комментарий после выделения. (Ctrl+L)
+ */
+
+"use strict";
+
+//Создадим 4 произвольных массива для будущего объекта
+function makeCar() {
+
+var makes = ["Toyota", "Fiat", "Mazda", "Vaz", "BMW", "Audi"];
+var models = ["Prado", "Albea", "CX-7", "Patriot", "X6", "A5"];
+var years = [2010, 2005, 2015, 2008, 2018, 2019];
+var colors = ["red", "blue", "green", "yellow", "white", "black"];
+var converiable = [true, false];
+
+//Далее сгенерируем случайным образом комбинации массивов
+
+var rand1 = Math.floor(Math.random() * makes.length);
+var rand2 = Math.floor(Math.random() * models.length);
+var rand3 = Math.floor(Math.random() * years.length);
+var rand4 = Math.floor(Math.random() * colors.length);
+//Случайное число будет задавать кол-во пассажиров
+var rand5 = Math.floor(Math.random() * 5) + 1;
+//Случайное число для определения откидного верха
+var rand6 = Math.floor(Math.random() * 2);
+
+//А вот и наш объект car
+
+var car = {
+  make: makes[rand1],
+  model: models[rand2],
+  year: years[rand3],
+  color: colors[rand4],
+  passangers: rand5,
+  converiable: converiable[rand6],
+  mileage: 0
+};
+return car;
+}
+
+function displayCar(car) {
+  alert("Это новая машина " + car.make + " " + car.year + " " + car.model + " с пробегом " + car.mileage + " километров" + " цвета " + car.color);
+}
+var carToSell = makeCar();
+displayCar(carToSell);
+
